3ac15625af1198485db8838c879c37286cecd6ce
orenor10 <orenor10@barak.net.il>
2016-03-20 22:19:12 +0200

    add modal service

---
+++ src/components/modal/modalService.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function (angular) {
+
+    function ModalService() {
+
+        var baseTemplateUrl;
+
+        this.setBaseTemplatePath = function(templateUrl) {
+            baseTemplateUrl = templateUrl;
+        };
+
+        this.$get = ['$mdDialog', function($mdDialog) {
+            var ModalService = {};
+
+            ModalService.showBaseModal = function (popupData) {
+                // @todo: how we pass the override css class??
+                //this.overrideCssClass = popupData.overrideCssClass;
+
+                $mdDialog.show({
+                    locals: {
+                        svgIcon: popupData.svgIcon,
+                        innerTemplateUrl: popupData.innerTemplateUrl
+                    },
+                    bindToController: true,
+                    controller: popupData.controllerName,
+                    controllerAs: 'vm',
+                    templateUrl: baseTemplateUrl,
+                    clickOutsideToClose: true,
+                    escapeToClose: true
+                });
+            };
+
+            return ModalService;
+        }];
+    }
+
+    angular.module('znk.infra-dashboard.modal').provider('ModalService', ModalService);
+
+})(angular);
+
+
