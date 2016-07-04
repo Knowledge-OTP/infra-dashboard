0ad5953c33d093b11a6141c1a2a6cd7ea6a561bb
stormcrow85 <ran.shternin@gmail.com>
2016-07-04 12:08:48 +0300

    - removed jQuery

--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
             'bower_components/firebase/firebase.js',
             'bower_components/angular-animate/angular-animate.js',
             'bower_components/Swiper/dist/js/swiper.js',
+            'bower_components/ng-idle/angular-idle.js',
             'bower_components/infra/dist/znk.infra.js',
             'bower_components/angular-translate/angular-translate.js',
             'bower_components/angular-translate-loader-partial/angular-translate-loader-partial.js',
