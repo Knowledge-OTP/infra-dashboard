c125c71b8bc141a3f9e3bfafb9b41140ce3b8ce5
assafhp <assafshp@zahav.net.il>
2016-07-19 12:49:47 +0300

    initial

--- dist/groups/groups.js
+++ dist/groups/groups.js
@@ -1,7 +1,7 @@
 (function (angular) {
     'use strict';
 
-    angular.module('znk.infra-dashboard.groups', []);
+    angular.module('znk.infra-dashboard.groups', ['znk.infra.storage']);
 })(angular);
 
 (function (angular) {
@@ -18,14 +18,14 @@
                 AuthSrvName = authServiceName;
             };
 
-            this.$get = ['$injector', 'ENV', '$q', '$log', function($injector, ENV, $q, $log) {
+            this.$get = ['$injector', 'ENV', '$q', '$log', 'StorageSrv', function($injector, ENV, $q, $log, StorageSrv) {
 
                 var GroupsService = {
                     defaultGroupName: 'assorted'
                 };
                 var authSrv = $injector.get(AuthSrvName);
                 var storageSrv = $injector.get(StorageSrvName);
-                var GROUPS_PATH = storageSrv.variables.appUserSpacePath + '/groups';
+                var GROUPS_PATH = StorageSrv.variables.appUserSpacePath + '/groups';
                 var groupChangeCbArr = [];
                 var groupChildAddedCbArr = [];
 
