9c6ec6cd126ff2699947955d94abb3bcf41ae060
stormcrow85 <ran.shternin@gmail.com>
2016-08-15 11:15:53 +0300

    - moved StudentContextSrv out - dist

--- dist/main.js
+++ dist/main.js
@@ -466,58 +466,6 @@ angular.module('znk.infra-dashboard.modal').run(['$templateCache', function($tem
 (function (angular) {
     'use strict';
 
-    angular.module('znk.infra-dashboard.userContext', []);
-})(angular);
-
-(function (angular) {
-    'use strict';
-
-    angular.module('znk.infra-dashboard.userContext').service('StudentContextSrv', ['$window', '$log',
-
-        function ($window, $log) {
-            var StudentContextSrv = {};
-
-            var _storageStudentUidKey = 'currentStudentUid';
-            var _currentStudentUid = '';
-
-            StudentContextSrv.getCurrUid = function () {
-                if (_currentStudentUid.length === 0) {
-                    if ($window.sessionStorage) {
-                        var storedCurrentUid = $window.sessionStorage.getItem(_storageStudentUidKey);
-                        if (storedCurrentUid) {
-                            _currentStudentUid = storedCurrentUid;
-
-                        } else {
-                            $log.error('StudentContextSrv: no student uid');
-                        }
-                    } else {
-                        $log.error('StudentContextSrv: no student uid');
-                    }
-                }
-                return _currentStudentUid;
-
-            };
-
-            StudentContextSrv.setCurrentUid = function (uid) {
-                _currentStudentUid = uid;
-
-                if ($window.sessionStorage) {
-                    $window.sessionStorage.setItem(_storageStudentUidKey, uid);
-                }
-            };
-
-            return StudentContextSrv;
-        }
-    ]);
-})(angular);
-
-angular.module('znk.infra-dashboard.userContext').run(['$templateCache', function($templateCache) {
-
-}]);
-
-(function (angular) {
-    'use strict';
-
     angular.module('znk.infra-dashboard.userResults', []);
 })(angular);
 
